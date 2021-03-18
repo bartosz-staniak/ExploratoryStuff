0a31214543c00f81752b01f36a3ab0581f6775e4
bs <bartosz.staniak@gmail.com>
2021-03-19 00:23:33 +0100

    moved the scripts to a separate file

---
+++ Scripts.js
@@ -0,0 +1,64 @@
+function getOpinion() {
+    var opinion = document.getElementById('dataList').value;
+    var hideousButton = document.getElementById("button1");
+    if (opinion == "nice!") {
+        hideousButton.style.display = "none";
+    } else  if (opinion == "okay.") {
+        hideousButton.style.display = "none";
+    } else {
+        hideousButton.style.display = "block";
+    }
+}
+
+function hideShow() {
+    var onOff = document.getElementById("remove_it");
+    var buttonName = document.getElementById("button1");
+    if (onOff.style.display === "none") {
+        onOff.style.display = "block";
+        buttonName.innerHTML = "It is still hideous"
+    } else {
+        onOff.style.display = "none";
+        buttonName.innerHTML = "Oh well, bring it back";
+    }
+    
+}
+
+function hideShowImg() {
+    var onOff = document.getElementById("image");
+    var buttonName = document.getElementById("buttonImage");
+    if (onOff.style.display === "none") {
+        onOff.style.display = "block";
+        buttonName.innerHTML = "Hide this image"
+    } else {
+        onOff.style.display = "none";
+        buttonName.innerHTML = "Show this image";
+    }
+    
+}
+
+function storeReport() {
+    if (typeof(Storage) !== "undefined") {
+        localStorage.reportHistory += document.getElementById
+        ("reportHere").value + " ";
+        document.getElementById("reportHere").value = "";
+        
+    } else {
+        document.getElementById("readReport").innerHTML = "Error";
+        return;
+    }
+    document.getElementById("readReport")
+            .innerHTML = "" + localStorage.reportHistory + " ";
+}
+
+function deleteReport() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.reportHistory) {
+            localStorage.reportHistory = "";
+        }
+    } else {
+        document.getElementById("readReport").innerHTML = "Error";
+        return;
+    }
+    document.getElementById("readReport")
+    .innerHTML = localStorage.reportHistory;
+}
