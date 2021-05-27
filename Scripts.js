75b11b727a4f496b51c564992f80d9878f09964c
bs <bartosz.staniak@gmail.com>
2021-05-27 22:50:41 +0200

    introduced a toggleDebug variable

--- Scripts.js
+++ Scripts.js
@@ -134,5 +134,5 @@ $.getJSON("https://localhost:44391/api/weatherforecast/GetWhateverItReturns",
 });
 
 function hideDebug () {
-    
+    var toggleDebug = document.getElementById("hideDebug");
 }
