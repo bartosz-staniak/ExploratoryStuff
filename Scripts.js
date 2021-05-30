1243f2e28e60e296617e2600114abfedf680e5ed
bs <bartosz.staniak@gmail.com>
2021-05-30 02:19:54 +0200

    removed the [ char fromt he condition

--- Scripts.js
+++ Scripts.js
@@ -158,7 +158,7 @@ function getWhatever (method) {
 
             var ichar = 0;
             while (request.response[ichar] != null) {
-                if (request.response[ichar] != '{' | '['){
+                if (request.response[ichar] != '{'){
                     
                 }
                 toggleDisplayGetAll.innerHTML +=
