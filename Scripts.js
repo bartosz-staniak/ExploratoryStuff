2b818c42452a0aa512c422a03da1100b360ec14d
bs <bartosz.staniak@gmail.com>
2021-06-13 02:47:46 +0200

    commented out an instruction in order to troubleshoot an issue

--- Scripts.js
+++ Scripts.js
@@ -249,7 +249,7 @@ function getOneRecord() {
     if (document.getElementById("readRecord").innerHTML.value == initialParagraphValue) {
         document.getElementById("readRecord").innerHTML
     = "There is no such record.";
-    modifyRecordBtn.style.display = "none";
+    // modifyRecordBtn.style.display = "none";
 
     }
 }
