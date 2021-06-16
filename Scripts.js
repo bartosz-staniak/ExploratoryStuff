38cf677676defd08ffa5ad2b6cf8a5ffa5ddecf0
bs <bartosz.staniak@gmail.com>
2021-06-17 01:57:24 +0200

    added a new function

--- Scripts.js
+++ Scripts.js
@@ -251,6 +251,9 @@ function getOneRecord() {
         modifyRecordBtn.style.display = "none";
         updateRecordInputs.style.display = "none";
         Timeout(5000);
+        function checkAgain() {
+            
+        }
         if (document.getElementById("readRecord").innerHTML.value == "Requesting data...") {
             document.getElementById("readRecord").innerHTML = "There is no such record.";
             modifyRecordBtn.style.display = "none";
