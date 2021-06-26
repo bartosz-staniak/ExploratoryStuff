cafe95c24a41834f79474fe5334a8fe74b443f9e
bs <bartosz.staniak@gmail.com>
2021-06-26 02:38:57 +0200

    another attempt

--- Scripts.js
+++ Scripts.js
@@ -15,7 +15,7 @@ function getOpinion() {
 }
 
 function removeOpinion() {
-    document.getElementById('datalist').innerText = " ";
+    document.getElementById('datalist').remove();
 }
 
 function hideShow() {
