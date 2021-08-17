9462078cc2c46e779065231d66cad344b56ccaad
bs <bartosz.staniak@gmail.com>
2021-08-18 00:55:56 +0200

    yet another failed attempt

--- Scripts.js
+++ Scripts.js
@@ -18,7 +18,7 @@ window.onload = function () {
     for (let i = 0; i < totalRedundantElements; i++) {
         redundantElements[i].style.display = "none";
     }
-    document.getElementById('currentDateTime').innerHTML = Date();
+    document.getElementById('currentDateTime').innerHTML = Date().toDateString();
 }
 
 function toggleRedundant() {
