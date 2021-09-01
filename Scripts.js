1526191e2bafde77b9057ee8d3c99a9fbdaad583
bs <bartosz.staniak@gmail.com>
2021-09-01 03:51:29 +0200

    attempt to add the magnifying instructions dynamically

--- Scripts.js
+++ Scripts.js
@@ -420,5 +420,6 @@ function showImageDiv() {
 setTimeout(showImageDiv, 5000); // most of the tests failed after this change 
 
 function demagnifyElements(){
-    document.styleSheets[0].insertRule('input { color:red;');
+    document.styleSheets[0]
+    .insertRule('*:hover:not(body):not(html):not(div) {transform: scale(2,3);}');
 }
