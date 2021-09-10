4d958e32c1caeffbace837cd73fac0c858ff2f99
bs <bartosz.staniak@gmail.com>
2021-09-10 03:28:01 +0200

    changed the way resource is displayed

--- Scripts.js
+++ Scripts.js
@@ -295,7 +295,7 @@ function getWhatever (method) {
 function getLocations(){
     const url = 'https://localhost:44391/api/another';
     fetch (url)
-    .then(resource => resource.json())
+    .then(resource => resource.text())
     .then(resource => {
         locationsDisplayParagraph.innerHTML = JSON.stringify(resource);
     }
