169d924ad1ad6229af29ca61163fd331b3bb142c
bs <bartosz.staniak@gmail.com>
2021-09-12 22:37:02 +0200

    assigned striginfied response to a variable

--- Scripts.js
+++ Scripts.js
@@ -301,7 +301,7 @@ function getLocations(){
         for (let i=0; i<parsedResource.length; i++){
             locationsDisplayParagraph.innerHTML = "" + parsedResource[i].location;
         } */
-
+        var strigifiedResource = JSON.stringify(resource);
         locationsDisplayParagraph.innerHTML = JSON.stringify(resource);
         // locationsDisplayParagraph.innerHTML = resource[0]; // it prints [object Object]
     }
