1c00688ad554c2f9a01a929cd0530fc0a0618815
bs <bartosz.staniak@gmail.com>
2021-09-19 23:28:14 +0200

    an attempt to print the first row of data

--- Scripts.js
+++ Scripts.js
@@ -335,7 +335,8 @@ function getLocations(){
 function sortTable() {
     let row, tableBody;
     tableBody = getElementById("locationsData");
-}
+    row = document.getElementsByTagName("tr")[0];
+    console.log(row);
 
 function toggleResource () {
     var toggleButtonName = document.getElementById("toggleResourceBtn");
