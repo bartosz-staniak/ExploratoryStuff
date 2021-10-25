9e26139f1bff75e8631d99bbdffdf202af32545a
bs <bartosz.staniak@gmail.com>
2021-10-25 03:10:32 +0200

    a failed attempt to slide contents down on mouse click

--- Scripts.js
+++ Scripts.js
@@ -212,7 +212,7 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function(){
-    $("html").mouseup(function(){
+    $("initialDiv").mouseup(function(){
         // $("#slide_it").slideDown(2000); // bug on sliding down
         $("#slide_it_too").slideDown(2000);
     });
