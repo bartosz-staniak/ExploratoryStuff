a1b911de6d6ce9e3bac3adc636dd358e061f7eb9
bs <bartosz.staniak@gmail.com>
2021-10-25 21:28:54 +0200

    a failed attempt to fix adding multiple words

--- Scripts.js
+++ Scripts.js
@@ -450,7 +450,7 @@ function updateRecord() {
         return;
     }
     
-    if (document.getElementById("PostOneSubmittedBy").value.match(/^\s.|^\s+|.\s+./g)){
+    if (document.getElementById("PostOneSubmittedBy").value.match(/^\s.|^\s+|.[\s]+./g)){
         return;
     }
     else if (document.getElementById("PostOneSubmittedBy").value == "") {
