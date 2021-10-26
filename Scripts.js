cff9eb31c8e37599b281c510558f73dfd1a88c4d
bs <bartosz.staniak@gmail.com>
2021-10-26 07:43:10 +0200

    failed attempt to filter out trailing spaces as it blocks multiple words

--- Scripts.js
+++ Scripts.js
@@ -430,7 +430,7 @@ function modifyRecord() {
 // --- API PUT function start ---
 
 function updateRecord() {
-    var namesValidationRegex = /^\s.|^\s+|.[\s][\s+]./g;
+    var namesValidationRegex = /^\s.|^\s+|.[\s][\s+].|.\s/g;
     var putRequest = new XMLHttpRequest();
     if (document.getElementById("getOneId").value == ""){
         return;
