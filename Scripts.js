af57754d91f7c2dfbe0224028026dd723a4dde19
bs <bartosz.staniak@gmail.com>
2021-10-28 11:11:02 +0200

    commented out the re-introduced line of code which is meant to serve as a reminder of the differences in structure

--- Scripts.js
+++ Scripts.js
@@ -295,7 +295,7 @@ function getWhatever (method) {
 
         const url = 'https://localhost:44391/api/weatherforecast/GetWhateverItReturns';
         fetch (url)
-        .then(resource => resource.json())
+        // .then(resource => resource.json())
         /*.then(resource => {
             return resource.json()
         })*/
