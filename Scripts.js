2e75f174f6df72d49b453179047f001d1267185c
bs <bartosz.staniak@gmail.com>
2021-12-03 06:33:31 +0100

    magnification partial fix

--- Scripts.js
+++ Scripts.js
@@ -739,7 +739,7 @@ function demagnifyElements(){
         document.styleSheets[0]
         .insertRule
         //('*:hover:not(body):not(html):not(li):not(div) {transform: scale(2,3); background: white}');
-        ('::selection:hover {transform: scale(2,3); background: white}');
+        ('*:empty:hover {transform: scale(2,3); background: white}');
 
         magnificationButton.innerHTML = 'Disable magnification';
     } else {
