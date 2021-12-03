fce12170c2fa86766da7977bd60e0f49bac7436c
bs <bartosz.staniak@gmail.com>
2021-12-03 06:11:09 +0100

    reenabled div and body magnification

--- Scripts.js
+++ Scripts.js
@@ -739,7 +739,7 @@ function demagnifyElements(){
         document.styleSheets[0]
         .insertRule
         //('*:hover:not(body):not(html):not(li):not(div) {transform: scale(2,3); background: white}');
-        ('*:hover:not(html):not(*>*):not(body):not(div) {transform: scale(2,3); background: white}');
+        ('*:hover:not(html):not(*>*) {transform: scale(2,3); background: white}');
 
         magnificationButton.innerHTML = 'Disable magnification';
     } else {
