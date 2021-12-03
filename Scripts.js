44d0d3376830555aa87e5cbbcc54bc3e07cd2aa7
bs <bartosz.staniak@gmail.com>
2021-12-03 07:28:38 +0100

    removed a redundant line of code

--- Scripts.js
+++ Scripts.js
@@ -738,7 +738,6 @@ function demagnifyElements(){
     if (magnificationState == "Enable") {
         document.styleSheets[0]
         .insertRule
-        //('*:hover:not(body):not(html):not(li):not(div) {transform: scale(2,3); background: white}');
         ('*:hover:not(body, div, form, html, li, ul, p input) {transform: scale(2,3); background: white}');
 
         magnificationButton.innerHTML = 'Disable magnification';
