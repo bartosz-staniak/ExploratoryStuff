ebde798393ef0431f9ed52491297a375e9bdfc76
bs <bartosz.staniak@gmail.com>
2024-09-20 21:42:29 +0200

    changed the number to try to see what it stood for

--- Scripts.js
+++ Scripts.js
@@ -768,7 +768,7 @@ function doSomething() {
         console.log("Did something");
         // The fulfillment value of the promise
         resolve("https://example.com/");
-      }, 200);
+      }, 20000);
     });
   }
   
