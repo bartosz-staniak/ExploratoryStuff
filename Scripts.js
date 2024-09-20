ec1be6b8e6f1be2c7fceee7052c5b9cc12dc7d9e
bs <bartosz.staniak@gmail.com>
2024-09-21 01:34:35 +0200

    added an if function with a condition concerning status '0'

--- Scripts.js
+++ Scripts.js
@@ -530,6 +530,9 @@ function getOneRecord() {
             }
     }).fail(function(jqXHR, textStatus, errorThrown) { // revisit detailed error printing later
         document.getElementById("readRecord").style = "Color:red"
+        if (jqXHR.status == "0") {
+            
+        }
         else if (jqXHR.status == "404") {
             document.getElementById("readRecord").innerHTML = "Request failed. Record not found."
         }
