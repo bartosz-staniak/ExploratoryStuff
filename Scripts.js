547f0f27ef6fa9032630d223cdd89f98fbf9386d
bs <bartosz.staniak@gmail.com>
2024-09-20 04:15:45 +0200

    nested a value within the template literal

--- Scripts.js
+++ Scripts.js
@@ -527,7 +527,7 @@ function getOneRecord() {
                 document.getElementById("getOneSummary").value = record.summary;
             }
     }).fail(function(jqXHR, textStatus, errorThrown) { // revisit detailed error printing later
-        document.getElementById("readRecord").innerHTML = `Request failed. ${}` // + errorThrown
+        document.getElementById("readRecord").innerHTML = `Request failed. ${errorThrown}` // + errorThrown
     });
     if (document.getElementById("readRecord").innerHTML == initialParagraphValue) {
         document.getElementById("readRecord").innerHTML = "Requesting data...";
