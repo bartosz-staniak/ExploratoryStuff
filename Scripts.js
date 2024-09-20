207895df33aa22b950df7497dc55df50b7bd4b8a
bs <bartosz.staniak@gmail.com>
2024-09-20 06:32:52 +0200

    added a comment

--- Scripts.js
+++ Scripts.js
@@ -533,7 +533,7 @@ function getOneRecord() {
             document.getElementById("readRecord").innerHTML = "Request failed. Record not found."
         }
         else
-        document.getElementById("readRecord").innerHTML = "Request failed due to " + jqXHR.status
+        document.getElementById("readRecord").innerHTML = "Request failed due to " + jqXHR.status // untested
     });
     if (document.getElementById("readRecord").innerHTML == initialParagraphValue) {
         document.getElementById("readRecord").innerHTML = "Requesting data...";
